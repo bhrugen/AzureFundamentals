99539f20bada59c3f83881a4c0f76dd6deeb7107
Bhrugen Patel <bhrugen1990@gmail.com>
2021-08-28 16:05:11 -0500

    Section 2 - Managing Containers

--- AzureBlobProject/Controllers/ContainerController.cs
+++ AzureBlobProject/Controllers/ContainerController.cs
@@ -1,4 +1,5 @@
-﻿using AzureBlobProject.Services;
+﻿using AzureBlobProject.Models;
+using AzureBlobProject.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzureBlobProject.Controllers;
@@ -15,5 +16,23 @@ public class ContainerController : Controller
     {
         var allContainer = await _containerService.GetAllContainer();
         return View(allContainer);
+    }
+
+    public async Task<IActionResult> Delete(string containerName)
+    {
+        await _containerService.DeleteContainer(containerName);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        return View(new Container());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Container container)
+    {
+        await _containerService.CreateContainer(container.Name);
+        return RedirectToAction(nameof(Index));
     }
 }
