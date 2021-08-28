5da7cfa1992d8d7eda8aa606ccf69cd964c661f3
Bhrugen Patel <bhrugen1990@gmail.com>
2021-08-28 15:52:01 -0500

    Section 2 - List All Controller Index View

---
+++ AzureBlobProject/Controllers/ContainerController.cs
@@ -0,0 +1,19 @@
+﻿using AzureBlobProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobProject.Controllers;
+public class ContainerController : Controller
+{
+    private readonly IContainerService _containerService;
+
+    public ContainerController(IContainerService containerService)
+    {
+        _containerService = containerService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var allContainer = await _containerService.GetAllContainer();
+        return View(allContainer);
+    }
+}
---
+++ AzureBlobProject/Models/Container.cs
@@ -0,0 +1,9 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureBlobProject.Models;
+public class Container
+{
+    [Required]
+    public string Name {  get; set; }   
+}
