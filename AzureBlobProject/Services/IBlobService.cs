1f6ad2ea71b0ec7601a17eadb417b56d1f6735b5
Bhrugen Patel <bhrugen1990@gmail.com>
2021-08-28 16:10:59 -0500

    Section 2 - Blob Service Interface

---
+++ AzureBlobProject/Services/IBlobService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AzureBlobProject.Services;
+
+public interface IBlobService
+{
+    Task<string> GetBlob(string name,string containerName);
+    Task<List<string>> GetAllBlobs(string containerName);
+    Task<bool> UploadBlob(string name, IFormFile file, string containerName);
+    Task<bool> DeleteBlob(string name,string containerName);
+}
