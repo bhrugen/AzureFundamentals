b6703170b2f50d9b026b6892904db662a33f0352
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-03 15:18:01 -0500

    Section 5 - Azure function Timer Trigger

---
+++ AzureTangyFunc/UpdateStatusToCompletedAndSendEmail.cs
@@ -0,0 +1,43 @@
+using System;
+using AzureTangyFunc.Data;
+using AzureTangyFunc.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using SendGrid.Helpers.Mail;
+
+namespace AzureTangyFunc
+{
+    public  class UpdateStatusToCompletedAndSendEmail
+    {
+        private readonly AzureTangyDbContext _db;
+        public UpdateStatusToCompletedAndSendEmail(AzureTangyDbContext db)
+        {
+            _db = db;
+        }
+
+        [FunctionName("UpdateStatusToCompletedAndSendEmail")]
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
+            [SendGrid(ApiKey = "CustomSendGridKeyAppSettingName")] IAsyncCollector<SendGridMessage> messageCollector, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            IEnumerable<SalesRequest> salesRequestFromDb = _db.SalesRequests.Where(u => u.Status == "Image Processed");
+            foreach (var salesReq in salesRequestFromDb)
+            {
+                //for each request update status
+                salesReq.Status = "Completed";
+            }
+
+            _db.UpdateRange(salesRequestFromDb);
+            _db.SaveChanges();
+
+            var message = new SendGridMessage();
+            message.AddTo("dotnetmastery@gmail.com");
+            message.AddContent("text/html", $"Processing completed for {salesRequestFromDb.Count()} records");
+            message.SetFrom(new EmailAddress("hello@dotnetmastery.com"));
+            message.SetSubject("Azure Tangy Processing Successful");
+            await messageCollector.AddAsync(message);
+        }
+    }
+}
