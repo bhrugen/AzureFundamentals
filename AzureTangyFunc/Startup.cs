a5cf1d5798e71c89c195c4b7bc6d5f424a1cac64
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-03 13:20:11 -0500

    Section 5 - Configure Dependency Injection in Azure Function

---
+++ AzureTangyFunc/Startup.cs
@@ -0,0 +1,28 @@
+﻿using AzureTangyFunc;
+using AzureTangyFunc.Data;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace AzureTangyFunc
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureSqlDatabase");
+
+            builder.Services.AddDbContext<AzureTangyDbContext>(
+                options => options.UseSqlServer(connectionString));
+
+            builder.Services.BuildServiceProvider();
+        }
+    }
+}
