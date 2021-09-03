c0ffcb394f9fdb645fd2917733c7e2909a35ccdb
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-03 13:32:14 -0500

    Section 5 - Azure Function Complete Flow in Action

--- AzureTangyFunc/OnQueueTriggerUpdateDatabase.cs
+++ AzureTangyFunc/OnQueueTriggerUpdateDatabase.cs
@@ -1,17 +1,33 @@
 using System;
+using AzureTangyFunc.Data;
+using AzureTangyFunc.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace AzureTangyFunc
 {
-    public static class OnQueueTriggerUpdateDatabase
+    public class OnQueueTriggerUpdateDatabase
     {
+        private readonly AzureTangyDbContext _db;
+
+        public OnQueueTriggerUpdateDatabase(AzureTangyDbContext db)
+        {
+            _db = db;
+        }
+
+
         [FunctionName("OnQueueTriggerUpdateDatabase")]
-        public static void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobsStorage")]string myQueueItem, 
+        public void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobsStorage")]SalesRequest myQueueItem, 
             ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            myQueueItem.Status = "Submitted";
+            _db.SalesRequests.Add(myQueueItem);
+            _db.SaveChanges();
+
+
         }
     }
 }
