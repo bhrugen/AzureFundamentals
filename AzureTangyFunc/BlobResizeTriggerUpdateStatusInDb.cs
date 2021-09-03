3e95f6f1c4603b0d919218a7c040d6aa5dc45d33
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-03 14:59:22 -0500

    Section 5 - Update Status in Database on Resize Function

---
+++ AzureTangyFunc/BlobResizeTriggerUpdateStatusInDb.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using AzureTangyFunc.Data;
+using AzureTangyFunc.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AzureTangyFunc
+{
+    public class BlobResizeTriggerUpdateStatusInDb
+    {
+        private readonly AzureTangyDbContext _db;
+        public BlobResizeTriggerUpdateStatusInDb(AzureTangyDbContext db)
+        {
+            _db = db;
+        }
+
+        [FunctionName("BlobResizeTriggerUpdateStatusInDb")]
+        public void Run([BlobTrigger("functionsalesrep-sm/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, 
+            string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            var fileName = Path.GetFileNameWithoutExtension(name);
+            SalesRequest salesRequestFromDb = _db.SalesRequests.FirstOrDefault(u => u.Id == fileName);
+            if (salesRequestFromDb != null)
+            {
+                salesRequestFromDb.Status = "Image Processed";
+                _db.SalesRequests.Update(salesRequestFromDb);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
