c232edb400661b94c968c8222aa0e458c0d10179
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-03 11:34:22 -0500

    Section 5 - Azure Function - Http to Queue

---
+++ AzureTangyFunc/Models/SalesRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTangyFunc.Models
+{
+    public class SalesRequest
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+    }
+}
--- AzureTangyFunc/OnSalesUploadWriteToQueue.cs
+++ AzureTangyFunc/OnSalesUploadWriteToQueue.cs
@@ -1,3 +1,4 @@
+using AzureTangyFunc.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -15,20 +16,19 @@ namespace AzureTangyFunc
         [FunctionName("OnSalesUploadWriteToQueue")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Queue("SalesRequestInBound",Connection ="AzureWebJobsStorage")] IAsyncCollector<SalesRequest> salesRequestQueue,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Sales Request received by OnSalesUploadWriteToQueue function");
 
-            string name = req.Query["name"];
+           
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            SalesRequest data = JsonConvert.DeserializeObject<SalesRequest>(requestBody);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            await salesRequestQueue.AddAsync(data);
 
+            string responseMessage = "Sales Request has been received for ." + data.Name;
             return new OkObjectResult(responseMessage);
         }
     }
