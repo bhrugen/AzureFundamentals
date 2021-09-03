0731cba9518e65f97fb81c60ff88df742fec31db
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-03 08:21:28 -0500

    Section 4 - Create Azure Function Project

---
+++ AzureFunctionTangyWeb/Models/SalesRequest.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AzureFunctionTangyWeb.Models;
+public class SalesRequest
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Status { get; set; }
+}
