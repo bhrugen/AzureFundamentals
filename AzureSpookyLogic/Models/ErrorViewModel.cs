71211f4a3d5bf206fec8fe509e15cd8c82ea8442
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-05 11:59:11 -0500

    Section 7 - Azure Function Create Project

---
+++ AzureSpookyLogic/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+
+namespace AzureSpookyLogic.Models;
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ AzureSpookyLogic/Models/SpookyRequest.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureSpookyLogic.Models;
+public class SpookyRequest
+{
+    public string Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Phone { get; set; }
+}
